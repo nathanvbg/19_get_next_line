441244b82f1e0433de8e70f91679705bc2b9ae3b
Nathan Verbruggen <naverbru@mi-r1-p2.s19.be>
2022-02-03 10:35:11 +0100

    tout ok sauf empty file

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: naverbru <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 16:20:32 by naverbru          #+#    #+#             */
-/*   Updated: 2022/02/03 10:14:14 by naverbru         ###   ########.fr       */
+/*   Updated: 2022/02/03 10:33:31 by naverbru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,25 +43,29 @@ char	*ft_process(char **rest, int fd)
 	int		ret;
 	char	*line;
 	char	buf[BUFFER_SIZE + 1];
+	char	*tmp;
 
 	line = ft_strndup(*rest, '\n');
+	free(*rest);
 	ret = read(fd, &buf, BUFFER_SIZE);
 	buf[ret] = '\0';
 	while (ret > 0 && is_charset(buf) == 0)
 	{
+		tmp = line;
 		line = ft_strjoin(line, buf);
-		//printf("line = %s\n", line);
+		free(tmp);
 		ret = read(fd, &buf, BUFFER_SIZE);
 		buf[ret] = '\0';
-		//printf("buf = %s\n", buf);
 	}
-	printf("line = %s\n", line);
+	tmp = line;
 	line = ft_strjoin(line, buf);
+	free(tmp);
 	if (ret == 0 && ft_strlen(line) == 0)
+	{
+		free(line);
 		return (NULL);
+	}
 	*rest = ft_strndup(ft_strchr(buf, '\n'), '\0');
-	printf("line = %s\n", line);
-
 	return (line);
 }
 
@@ -72,12 +76,12 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0)
 		return (NULL);
-	//printf("rst_debut = %s\n", rest[fd]);
 	if (rest[fd] == NULL)
 		rest[fd] = ft_strndup("", '\0');
 	if (is_charset(rest[fd]) == 1)
 	{
 		line = ft_strndup(rest[fd], '\n');
+		free(rest[fd]);
 		rest[fd] = ft_strndup(ft_strchr(rest[fd], '\n'), '\0');
 		return (line);
 	}
@@ -85,6 +89,7 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
+/*
 int	main()
 {
 	int		fd;
@@ -103,3 +108,4 @@ int	main()
 	close(fd);
 	return (0);
 }
+*/
